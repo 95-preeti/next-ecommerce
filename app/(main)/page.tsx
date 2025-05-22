982a9fda0edafb5a7d8c3e00dd2fa3297897a76c
Preeti yadav <preetiyadav30430@gmail.com>
2025-05-22 20:06:30 +0530

    install next.js and tailwind css and create the first template.

---
+++ app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <div className="text-2xl font-extrabold text-red-600 tracking-wide drop-shadow-lg italic">
+      Homepage
+    </div>
+  )
+}
